68e8369840bea4fb3f8828b13569ffe5c873fdf7
yhsup <bzyzy66@outlook.com>
2026-02-06 00:20:06 +0800

    Update s3.ts

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -8,7 +8,7 @@ export function createS3Client() {
     const endpoint = env.S3_ENDPOINT;
     const accessKeyId = env.S3_ACCESS_KEY_ID;
     const secretAccessKey = env.S3_SECRET_ACCESS_KEY;
-    const forcePathStyle = env.S3_FORCE_PATH_STYLE === "true"
+    const forcePathStyle = env.S3_FORCE_PATH_STYLE === "true";
     return new S3Client({
         region: region,
         endpoint: endpoint,
