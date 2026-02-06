257dcfc4c0219633a88ece6bc9fbde3a38e0ca98
yhsup <bzyzy66@outlook.com>
2026-02-06 23:24:32 +0800

    Update _worker.ts

--- server/src/_worker.ts
+++ server/src/_worker.ts
@@ -9,18 +9,25 @@ import { friendCrontab } from "./services/friends";
 import { rssCrontab } from "./services/rss";
 import { CacheImpl } from "./utils/cache";
 import { dbToken, envToken } from "./utils/di";
-export type DB = DrizzleD1Database<typeof import("./db/schema")>
+import { StorageService } from './services/storage'; // 引入 StorageService
+
+export type DB = DrizzleD1Database<typeof import("./db/schema")>;
 
 export default {
     async fetch(
         request: Request,
         env: Env,
     ): Promise<Response> {
-        const db = drizzle(env.DB, { schema: schema })
-        Container.set(envToken, env)
-        Container.set(dbToken, db)
+        const db = drizzle(env.DB, { schema: schema });
+        Container.set(envToken, env);
+        Container.set(dbToken, db);
 
-        const exist = Container.has("cache")
+        // 注册 StorageService 到容器
+        const storageService = new StorageService();
+        Container.set('storageService', storageService);
+
+        // 初始化缓存
+        const exist = Container.has("cache");
         if (!exist) {
             Container.set("cache", new CacheImpl());
             Container.set("server.config", new CacheImpl("server.config"));
@@ -29,25 +36,31 @@ export default {
 
         return await new Elysia({ aot: false })
             .use(app())
-            .handle(request)
+            .handle(request);
     },
+
     async scheduled(
         _controller: ScheduledController | null,
         env: Env,
         ctx: ExecutionContext
     ) {
-        const db = drizzle(env.DB, { schema: schema })
-        Container.set(envToken, env)
-        Container.set(dbToken, db)
+        const db = drizzle(env.DB, { schema: schema });
+        Container.set(envToken, env);
+        Container.set(dbToken, db);
 
-        const exist = Container.has("cache")
+        // 注册 StorageService 到容器
+        const storageService = new StorageService();
+        Container.set('storageService', storageService);
+
+        // 初始化缓存
+        const exist = Container.has("cache");
         if (!exist) {
             Container.set("cache", new CacheImpl());
             Container.set("server.config", new CacheImpl("server.config"));
             Container.set("client.config", new CacheImpl("client.config"));
         }
 
-        await friendCrontab(env, ctx)
-        await rssCrontab(env)
+        await friendCrontab(env, ctx);
+        await rssCrontab(env);
     },
-}
+};
